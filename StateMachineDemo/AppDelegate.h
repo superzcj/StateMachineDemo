489edf41bb16e12baaef00af65345c4c10d1f27a
jiafei <295417878@qq.com>
2015-08-29 11:54:59 +0800

    Initial Commit

---
+++ StateMachineDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StateMachineDemo
+//
+//  Created by zhangchaojie on 15/8/29.
+//  Copyright (c) 2015年 zcj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StateMachineDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StateMachineDemo
+//
+//  Created by zhangchaojie on 15/8/29.
+//  Copyright (c) 2015年 zcj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
